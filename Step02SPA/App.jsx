4f354d5c5374bb67654cb9d1ec08ed7644115597
kimgura <oli999@naver.com>
2025-09-10 15:45:00 +0900

    Step02SPA 예제 추가함

--- Step02SPA/App.jsx
+++ Step02SPA/App.jsx
@@ -2,17 +2,18 @@
 
 import 'bootstrap/dist/css/bootstrap.css'
 import { useOutlet } from 'react-router-dom';
+import BsNavbar from './components/BsNavbar';
 
 function App() {
     //React Router v6  에서 제공하는 hook
     //현재 경로에 맞는 자식 route compoent 를 반환 한다 
     const currentOutlet=useOutlet();
-
-    return (
-        <div className="container">
-           {currentOutlet}
+    return <>
+        <BsNavbar/>
+        <div className="container pt-4" style={{marginTop:"50px"}}>
+            {currentOutlet}
         </div>
-    );
+    </>
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,9 @@
 // src/pages/Home.jsx
 
-import { NavLink } from "react-router-dom";
-
 function Home() {
     return (
         <>
             <h1>인덱스 페이지 입니다</h1>
-            <ul>
-                <li>
-                   <NavLink to="/study">공부하기</NavLink>
-                </li>
-                <li>
-                   <NavLink to="/play">놀러가기</NavLink>
-                </li>
-            </ul>
         </>
     );
 }
