d4e83497a5412ec8ac9e40f452f6060bae39c234
kimgura <oli999@naver.com>
2025-09-17 14:39:28 +0900

    spa 기본 구성함

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// src/pages/Home.jsx
+
+function Home() {
+    return (
+        <>
+            <h1>인덱스 페이지 입니다</h1>
+        </>
+    );
+}
+
+export default Home;
---
+++ src/router/index.jsx
@@ -0,0 +1,35 @@
+/* 
+    src/router/index.jsx 파일
+
+    - 파일명을 index.js or index.jsx 로 작성하면 외부에서 
+      router 폴더까지만 import 했을때 자동으로 index.js or index.jsx 
+      파일에서 export default 된 자원을 사용할수 있다
+    - index 는 약속된 파일명이다 
+*/
+
+import { createHashRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+
+// 페이지 routing 정보를 배열에 미리 저장해 둔다.
+const routes=[
+    // spring boot 서버에 넣어서 실행하면 최초 로딩될때  /index.html 경로로 로딩된다.
+    // 그럴때도  Home 컴포넌트가 활성화 될수 있도록 라우트 정보를 추가한다. 
+    {path:"/index.html", element: <Home/>},
+    {path:"/", element:<Home/>},   
+];
+
+//export 해줄 router 객체를 만든다
+const router = createHashRouter([{
+    path:"/",
+    element:<App/>,
+    children: routes.map((route)=>{
+        return {
+            index: route.path === "/", //자식의 path 가 "/" 면 index 페이지 역활을 하게 하기 
+            path: route.path === "/" ? undefined : route.path, // path 에 "/" 두개가 표시되지 않게  
+            element: route.element //어떤 컴포넌트를 활성화 할것인지 
+        }
+    })
+}]);
+
+export default router;
