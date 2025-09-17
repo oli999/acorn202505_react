c7701b6143e0b96da5505b4af8e83202c1ef6411
kimgura <oli999@naver.com>
2025-09-17 14:45:42 +0900

    main.jsx 에 redux store 설정함

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,46 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 // routing 정보를 담고 있는 router import
 import router from './router'
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux';
 
+// resux store 에서 관리될 state 의 초기값
+const initState={
+  userInfo:null,
+  loginModal:{
+    title:null,
+    show:false
+  }
+};
+
+// reducer 함수에서 사용할 handler object
+const handlers = {
+  LOGIN_MODAL:(state, action)=>({
+    ...state,
+    loginModal: action.payload
+  }),
+  USER_INFO:(state, action)=>({
+    ...state,
+    userInfo: action.payload
+  })
+};
+
+// handler object 를 사용하는 새로운 reducer 함수 
+const reducer = (state=initState, action)=>{
+  //전달된 action 을 수행할 handler 함수를 얻어낸다
+  const handler = handlers[action.type];
+  //handler 함수가 존재한다면 handler 함수가 리턴하는 state 를 리턴하고 아니면 
+  //원래 state 를 리턴한다.
+  return handler ? handler(state, action) : state;
+}
+
+//위에서 만든 reducer 함수를 사용하는 redux store (중앙 저장소) 만들기
+const store = createStore(reducer);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </StrictMode>,
 )
